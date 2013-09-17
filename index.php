baa736e998abcde680b11c4d2b3b980a30bda18b
Preethi <preethi_rajasekar16@yahoo.com>
2013-09-17 10:16:59 -0700

    Added main index file

---
+++ index.php
@@ -0,0 +1,36 @@
+<!Doctype html>
+<html>
+<head>
+	<meta charset='utf-8'>
+	<meta name="description" content="Dynamic programming Project 1">
+	
+</head>
+
+<body>
+<div id="MainDiv">
+	<div class="introPanel">
+		<h3>Preethi Doraisamy</h3>
+		<h4>CSCI E-15 Dynamic Web Applications</h4>
+		<h4>Project 1</h4>
+		
+	</div>
+	<div class="aboutPanel">
+		<h3>About Me</h3>
+		<p>Hey, This is Preethi Doraisamy, working as a .Net developer for Cloud based application. I did my Bachelors in Computer Science, and now a virtual student in this course. I like to say "My hobbies include Knitting, Gardening, Reading, Cooking, etc,." but Seasonal</p>
+	</div>
+	<div class="softwarePanel">
+		<p> I have used SubLime editor for this project
+		</p>
+		<h4>Primary Skills</h4>
+		<ul>
+			<li>C#</li>
+			<li>WPF</li>
+			<li>WCF</li>
+			<li>SQL</li>
+			<li>XML</li>
+			<li>ADO.net</li>
+		</ul>
+	</div>
+</div>
+</body>
+</html>
